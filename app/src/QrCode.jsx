49845626ad51d85cc0d505227301ab675226e30b
Sadhguna Aithal <sadhgunaithal@outlook.com>
2024-11-26 19:43:19 +0530

    qr coide wrkinh

--- app/src/QrCode.jsx
+++ app/src/QrCode.jsx
@@ -1,33 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import { Html5QrcodeScanner } from 'html5-qrcode';
+import axios from 'axios';
 
 const QRCodeScanner = () => {
-    const [scanned_text,TextScanner] = useState('')
+  const [scannedText, setScannedText] = useState('');
+  const [error, setError] = useState('');
+  const [data,setData] = useState({
+    admission_no: '',
+    branch: '',
+    s_name: '',
+    section: '',
+    usn: ''
+  })
+
+  const sendToBackend = async (message) => {
+    try {
+      const response = await axios.post("http://localhost:5000/mark-attendance", { data: message });
+      console.log('Backend Response:', response.data);
+      setData(response.data.data[0][0])
+      console.log(data)
+
+    } catch (err) {
+      console.error('Error sending data to the backend:', err);
+      setError('err');
+    }
+  };
+
   useEffect(() => {
     const scanner = new Html5QrcodeScanner(
-      "reader", 
+      'reader',
       { fps: 60, qrbox: 1000 }
     );
 
     scanner.render(
       (decodedText) => {
         console.log(`Decoded Text: ${decodedText}`);
-        TextScanner(decodedText);
+        setScannedText(decodedText); // Display the scanned data
+        sendToBackend(decodedText);
       },
-      (error) => {
-        console.error(`QR Scanning Error: ${error}`);
+      (qrError) => {
+        console.error(`QR Scanning Error: ${qrError}`);
       }
     );
 
-    return () => scanner.clear();
+    return () => {
+      scanner.clear().catch((error) => {
+        console.error('Error clearing scanner:', error);
+      });
+    };
   }, []);
 
   return (
     <>
-        <div id="reader" style={{ width: "25%" }} />
-        <span>{scanned_text}</span>
+      <div id="reader" style={{ width: '50%' }} />
+      {
+      scannedText && <p>
+          Marked Attendance for: <br/>
+          Admission No : {data.admission_no} <br/>
+          Name : {data.s_name} <br/>
+          USN : {data.usn} <br/>
+          Branch : {data.branch} <br/>
+          Section : {data.section} <br/>
+        </p>
+      }
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </>
-    );
+  );
 };
 
 export default QRCodeScanner;
